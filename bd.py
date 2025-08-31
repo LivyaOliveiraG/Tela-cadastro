ebb938466113209fc6d69c25e2eb56075abd5fd9
OliverGo <livyanarah@gmail.com>
2025-08-31 18:31:05 -0300

    tentado adiciona o bd

--- bd.py
+++ bd.py
@@ -1,18 +1,21 @@
 import mysql.connector
+from flask import Flask, render_template, request 
+
+
 
 conexao = mysql.connector.connect(
     host='localhost',
+    port = 3306,
     database='site',
     user='root',
     password='adim'
 )
 
-cusor = conexao.cursor()
+cursor = conexao.cursor()
 
 if conexao.is_connected():
     print("conectado")
 
 
-
+cursor.close()
 conexao.close()
-cusor.close()
